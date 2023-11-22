36aa4867ea0354404d98f098545dabf0b1a38916
berdikulov-571 <mygithub571@gmail.com>
2023-11-22 14:21:15 +0500

    UserDto

---
+++ WorkingApiWithFiles/DTOs/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkingApiWithFiles.DTOs
+{
+    public class UserDto
+    {
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public IFormFile Image { get; set; } = default!;
+    }
+}
