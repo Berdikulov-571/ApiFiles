124985edade894828670d4851b7196aaa5fad42c
berdikulov-571 <mygithub571@gmail.com>
2023-11-22 14:21:07 +0500

    ApplicationDbContext

---
+++ WorkingApiWithFiles/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorkingApiWithFiles.Entities;
+
+namespace WorkingApiWithFiles.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
