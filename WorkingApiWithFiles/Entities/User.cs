189f4189032532609c501a94e90b50a50f71e7e1
berdikulov-571 <mygithub571@gmail.com>
2023-11-22 14:21:33 +0500

    User Model

---
+++ WorkingApiWithFiles/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkingApiWithFiles.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string ImageUrl { get; set; } = default!;
+    }
+}
